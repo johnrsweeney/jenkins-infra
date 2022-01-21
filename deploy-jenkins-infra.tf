45a8b25d90d3e1c8ce4bda354f1a6bf96778dba9
johnrsweeney <john.r.swe@gmail.com>
2022-01-21 23:17:14 +0000

    Add block device for Jenkins data

--- deploy-jenkins-infra.tf
+++ deploy-jenkins-infra.tf
@@ -3,4 +3,9 @@ resource "aws_instance" "jenkins_server" {
   instance_type   = "t2.medium"
   key_name        = "sweeney_london"
   security_groups = ["CentOS 8 -x86_64- - with Updates HVM-2004_01-AutogenByAWSMP-"]
+  ebs_block_device {
+    device_name = "/dev/sdb"
+    volume_size = 10
+    tags        = {function = "jenkins_master_db"}
+  }
 }
