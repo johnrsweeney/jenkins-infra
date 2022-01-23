b754e71268de44e7def6cf333b18082aaad84c29
johnrsweeney <john.r.swe@gmail.com>
2022-01-23 04:02:29 +0000

    Generate Ansible inventory from terraform

--- deploy-jenkins-infra.tf
+++ deploy-jenkins-infra.tf
@@ -9,3 +9,20 @@ resource "aws_instance" "jenkins_server" {
     tags        = {function = "jenkins_master_db"}
   }
 }
+
+resource "aws_instance" "jenkins_agent" {
+  ami             = "ami-08095fbc7037048f3"
+  instance_type   = "t2.medium"
+  key_name        = "sweeney_london"
+  security_groups = ["CentOS 8 -x86_64- - with Updates HVM-2004_01-AutogenByAWSMP-"]
+}
+
+resource "local_file" "jenkins_inventory" {
+  filename        = "${path.module}/jenkins_inventory"
+  file_permission = "0444"
+  content         = templatefile("${path.module}/jenkins_inventory.tftpl",
+                          {
+                            jenkins_server_ip = "${aws_instance.jenkins_server.public_ip}",
+                            jenkins_agent_ip  = "${aws_instance.jenkins_agent.public_ip}"
+                          })
+}
